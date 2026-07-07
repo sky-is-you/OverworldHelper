8cb129132fdb36c29a3994d064ee0b08d8de339c
sky-is-you <299973216+sky-is-you@users.noreply.github.com>
2026-07-07 18:33:48 -0400

    Actually mark GetConfig as obsolete
    messed up last time :D

--- Examples/CustomConfigExample/Source/OverworldHelperImports.cs
+++ Examples/CustomConfigExample/Source/OverworldHelperImports.cs
@@ -13,7 +13,9 @@ public static class OverworldHelperImports
     public static Action<Action<Overworld>> UnsubscribeFromOverworldCreated;
     public static Func<Overworld> GetOverworld;
 
-    [Obsolete("GetConfig is deprecated and will be removed in the next breaking update, use GetConfigFromArea or use GetConfigFromString")]    public static Func<AreaKey, Type, MapMeta> GetConfigFromArea;
+    [Obsolete("GetConfig is deprecated and will be removed in the next breaking update, use GetConfigFromArea or use GetConfigFromString")]
+	public static Func<AreaKey, Type, MapMeta> GetConfig;
+    public static Func<AreaKey, Type, MapMeta> GetConfigFromArea;
     public static Func<string, Type, MapMeta> GetConfigFromString;
 
     public static event Action<AreaKey> AreaChanged
