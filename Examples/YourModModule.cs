12f5900d9e4c036b5c1270f6148a778efb8bb727
sky-is-you <299973216+sky-is-you@users.noreply.github.com>
2026-07-07 17:10:09 -0400

    add null check to example module

--- Examples/YourModModule.cs
+++ Examples/YourModModule.cs
@@ -20,12 +20,16 @@ public class YourModModule : EverestModule {
     private void OnAreaChanged(AreaKey area)
     {
         CustomConfig.OurMapMeta meta = OverworldHelperImports.ReadConfig<CustomConfig.OurMapMeta>(area);
-        Logger.Info("YourMod","bool: "+meta.OurMeta.BoolValue);
-        Logger.Info("YourMod","float: "+meta.OurMeta.FloatValue);
-        Logger.Info("YourMod","posX: "+meta.OurMeta.PositionValue[0]);
-        Logger.Info("YourMod","posY: "+meta.OurMeta.PositionValue[1]);
-        Logger.Info("YourMod","posZ: "+meta.OurMeta.PositionValue[2]);
-        // use meta however youd like...
+        if (meta.OurMeta != null)
+        {
+            Logger.Info("YourMod", "bool: " + meta.OurMeta.BoolValue);
+            Logger.Info("YourMod", "float: " + meta.OurMeta.FloatValue);
+            Logger.Info("YourMod", "posX: " + meta.OurMeta.PositionValue[0]);
+            Logger.Info("YourMod", "posY: " + meta.OurMeta.PositionValue[1]);
+            Logger.Info("YourMod", "posZ: " + meta.OurMeta.PositionValue[2]);
+        }
+        else Logger.Warn("YourMod", "OurMeta not found in map config!");
+        // OR you can use meta however youd like...
     }
 
     public override void Load()
